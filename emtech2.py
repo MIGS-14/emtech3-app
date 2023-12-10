1892b173b48f5a0a658cd15f7e4781d7af71413f
MIGS-14 <153438405+MIGS-14@users.noreply.github.com>
2023-12-11 07:13:35 +0800

    Add files via upload

---
+++ emtech2.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import os
+from tensorflow import keras
+from keras.preprocessing import image as keras_image
+from keras.preprocessing.image import img_to_array
+import numpy as np
+
+# Load the saved model
+model_path = os.path.abspath('model_checkpoint.h5')
+if os.path.exists(model_path):
+    best_model = keras.models.load_model(model_path)
+
+# Function to preprocess an image for prediction
+def preprocess_image(img):
+    img_array = img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0  # Normalize the image
+
+    return img_array
+
+def predict(image):
+    # Preprocess the imagew
+    preprocessed_image = preprocess_image(image)
+
+    # Make predictions
+    predictions = best_model.predict(preprocessed_image)
+
+    # Get the predicted class (0 for cat, 1 for dog, assuming 2-class classification)
+    predicted_class = np.argmax(predictions)
+
+    return "Cat" if predicted_class == 0 else "Dog"
+
+def main():
+    st.title("Cat or Dog Predictor")
+
+    uploaded_file = st.file_uploader("Choose a file")
+
+    if uploaded_file is not None:
+        uploaded_img = keras_image.load_img(uploaded_file, target_size=(128, 128))
+        st.image(uploaded_img, caption="Uploaded Image", use_column_width=True)
+
+        prediction = predict(uploaded_img)
+
+        st.write(f"Prediction: {prediction}")
+
+
+if __name__ == "__main__":
+    main()
